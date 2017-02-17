13c21414a5220847906d6a1c49d9ff445598bdb0
Tessie Chao <mrxy56@Tessies-MacBook-Pro.local>
2017-02-17 22:15:24 +0800

    modify batch normalization

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -176,7 +176,13 @@ def batchnorm_forward(x, gamma, beta, bn_param):
     # the momentum variable to update the running mean and running variance,    #
     # storing your result in the running_mean and running_var variables.        #
     #############################################################################
-    pass
+    sample_mean = np.mean(x, axis = 0)
+    sample_var = np.var(x, axis = 0)
+    running_mean = momentum * running_mean + (1 - momentum) * sample_mean
+    running_var = momentum * running_var + (1 - momentum) * sample_var
+    new_x = (x - sample_mean) / np.sqrt(sample_var + eps)
+    out = new_x * gamma + beta
+    cache = (x, new_x)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
@@ -187,7 +193,9 @@ def batchnorm_forward(x, gamma, beta, bn_param):
     # and shift the normalized data using gamma and beta. Store the result in   #
     # the out variable.                                                         #
     #############################################################################
-    pass
+    new_x = (x - running_mean) / np.sqrt(running_var + eps)
+    out = new_x * gamma + beta
+    cache = (x, new_x)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
